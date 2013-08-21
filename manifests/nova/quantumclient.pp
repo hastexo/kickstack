263b613ffdea3d8ecc62d5aef5cf6344edb705d8
Dan Bode <bodepd@gmail.com>
2013-08-20 17:39:48 -0700

    refactor quantumclient
    lookup all data using hiera through the class
    interface.

--- manifests/nova/quantumclient.pp
+++ manifests/nova/quantumclient.pp
@@ -1,21 +1,21 @@
 #
 class kickstack::nova::quantumclient(
+  $auth_host             = hiera('auth_internal_address', '127.0.0.1'),
+  $quantum_auth_password = hiera('quantum_service_password'),
+  $quantum_auth_username = hiera('quantum_service_username', 'quantum'),
+  $quantum_host          = hiera('quantum_internal_address', '127.0.0.1'),
 ) inherits kickstack {
 
   include kickstack::nova::config
 
-  $keystone_internal_address = getvar("${fact_prefix}keystone_internal_address")
-  $quantum_admin_password = getvar("${fact_prefix}quantum_keystone_password")
-  $quantum_host = getvar("${fact_prefix}quantum_host")
-
   class { '::nova::network::quantum':
-    quantum_admin_password    => $quantum_admin_password,
+    quantum_admin_password    => $quantum_auth_password,
     quantum_auth_strategy     => 'keystone',
     quantum_url               => "http://${quantum_host}:9696",
-    quantum_admin_tenant_name => "$::kickstack::keystone_service_tenant",
-    quantum_region_name       => "$::kickstack::keystone_region",
-    quantum_admin_username    => 'quantum',
-    quantum_admin_auth_url    => "http://${keystone_internal_address}:35357/v2.0",
+    quantum_admin_tenant_name => $::kickstack::auth_service_tenant,
+    quantum_region_name       => $::kickstack::auth_region,
+    quantum_admin_username    => $quantum_auth_username,
+    quantum_admin_auth_url    => "http://${auth_host}:35357/v2.0",
     security_group_api        => 'quantum',
   }
 }
