226bb09132e1c2d1b9099eaed1020b03b38825cb
Dan Bode <bodepd@gmail.com>
2013-08-20 17:47:44 -0700

    remove node directory
    I'm not sure I have a good reason for removing this
    and may add back the same thing or something similar.

--- manifests/node/client.pp
+++
@@ -1,6 +0,0 @@
-class kickstack::node::client inherits kickstack {
-  include kickstack::keystone::client
-  include kickstack::glance::client
-#  include kickstack::cinder::client
-  include kickstack::quantum::client
-}
