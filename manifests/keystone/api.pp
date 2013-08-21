c7d413b8f1697864e1c0cadfcf4f04639d5f2532
Dan Bode <bodepd@gmail.com>
2013-08-20 17:38:36 -0700

    refactor keystone endpoint.
    - remove keystone endpoint (so it can be declared
    with the other endpoints)
    - move data lookup to class interface

--- manifests/keystone/api.pp
+++ manifests/keystone/api.pp
@@ -1,45 +1,44 @@
-class kickstack::keystone::api inherits kickstack {
-  include pwgen  
-
-  $admin_token = pick(getvar("${fact_prefix}keystone_admin_token"),pwgen())
-  $admin_password = pick(getvar("${fact_prefix}keystone_admin_password"),pwgen())
-  $admin_tenant = $::kickstack::keystone_admin_tenant
-  $sql_conn = getvar("${fact_prefix}keystone_sql_connection")
+#
+class kickstack::keystone::api(
+  $db_password      = hiera('keystone_db_password'),
+  $db_user          = hiera('keystone_db_user', 'keystone'),
+  $db_name          = hiera('keystone_db_name', 'keystone'),
+  $db_host          = hiera('db_host', '127.0.0.1'),
+  $db_type          = hiera('db_type', $::kickstack::db_type),
+  $admin_token      = hiera('admin_token'),
+  $admin_password   = hiera('admin_password'),
+  $admin_email      = hiera('admin_email', 'root@localhost'),
+  $admin_tenant     = hiera('admin_tenant', 'admin'),
+  $service_tenant   = hiera('service_tenant', $::kickstack::auth_service_tenant),
+  $package_ensure   = hiera('package_ensure', $::kickstack::package_ensure),
+  $verbose          = hiera('verbose', $::kickstack::verbose),
+  $debug            = hiera('verbose', $::kickstack::debug),
   $management_nic   = hiera('management_nic', $::kickstack::management_nic),
+) inherits kickstack {
+
+  $sql_connection_string = "${db_type}://${db_user}:${db_password}@${db_host}/${db_name}"
 
   class { '::keystone':
-    package_ensure => $::kickstack::package_ensure,
-    verbose        => $kickstack::verbose,
-    debug          => $kickstack::debug,
+    package_ensure => $package_ensure,
+    verbose        => $verbose,
+    debug          => $debug,
     catalog_type   => 'sql',
     admin_token    => $admin_token,
-    sql_connection => $sql_conn,
+    # TODO need to build a real value here
+    sql_connection => $sql_connection_string,
   }
 
-  # Installs the service user endpoint.
-  class { '::keystone::endpoint':
-    public_address   => "${hostname}${keystone_public_suffix}",
-    admin_address    => "${hostname}${keystone_admin_suffix}",
-    internal_address => $hostname,
-    region           => $keystone_region,
-    require      => Class['::keystone']
-  }
-
-  }
-
-  # Adds the admin credential to keystone.
   class { '::keystone::roles::admin':
-    email        => $kickstack::keystone_admin_email,
-    password     => $admin_password,
-    admin_tenant => $admin_tenant,
-    service_tenant => $kickstack::keystone_service_tenant,
-    require      => Class['::keystone::endpoint']
+    email          => $admin_email,
+    password       => $admin_password,
+    admin_tenant   => $admin_tenant,
+    service_tenant => $service_tenant,
   }
 
   file { '/root/openstackrc':
-    owner => root,
-    group => root,
-    mode => '0640',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
     content => template('kickstack/openstackrc.erb'),
     require => Class['::keystone::roles::admin']
   }
